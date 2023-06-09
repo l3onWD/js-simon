16df68279e0f9aa2c6b2caa523b429fca3aa9a15
l3on <l3onwebdev@gmail.com>
2023-06-09 16:30:57 +0200

    added unique random numbers feature

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ Bonus:
     - Mostriamo il countdown dei 30 secondi in pagina
     - Facciamo inserire i numeri all'utente tramite 5 campi di input invece che via prompt
 */
+
 /* -----------------------------------------
 * INIT
 -------------------------------------------*/
@@ -35,6 +36,52 @@ console.log('--- INIT DONE ---');
 * LOGIC
 -------------------------------------------*/
 
+//*** CLICK PLAY BUTTON ***//
+playBtn.addEventListener('click', () => {
 
+    /* --------
+    * FUNCTIONS
+    ----------*/
+
+    //*** GENERATE UNIQUE NUMBERS ***//
+    const generateUniqueNumbers = (number, minValue, maxValue) => {
+
+        const numbers = [];
+
+        // Populate list
+        while (numbers.length < number) {
+
+            let randomNumber;
+
+            // Get a random number until is unique
+            do {
+                randomNumber = Math.floor(Math.random() * (maxValue + 1 - minValue)) + minValue;
+
+            } while (numbers.includes(randomNumber));
+
+            // Add unique number inside the list
+            numbers.push(randomNumber);
+        }
+
+        return numbers;
+    }
+
+
+    /* --------
+    * LOGIC
+    ----------*/
+
+    // Hide Button
+    playBtn.classList.add('d-none');
+
+
+    // Generete 5 numbers from 1 to 20
+    const numbers = generateUniqueNumbers(5, 1, 20);
+    console.log(numbers);
+
+    // Show Panel
+    gamePanelElem.classList.remove('d-none');
+
+});
 
 
