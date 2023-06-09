0e39b86fb1e40bfc3fa7059ed678b8c58ac359ae
l3on <l3onwebdev@gmail.com>
2023-06-09 15:45:06 +0200

    added main html structure

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-/* -----------------------------------------
-! DEBUG
--------------------------------------------*/
-console.log('----------- DEBUG -----------');
+/*
+    - Visualizzare in pagina 5 numeri casuali diversi. Da lì parte un timer di 30 secondi.
+    - Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-console.log('JS OK!');// Check if JS is linked to the page
-
-
+Bonus:
+    - Mostriamo il countdown dei 30 secondi in pagina
+    - Facciamo inserire i numeri all'utente tramite 5 campi di input invece che via prompt
+*/
 /* -----------------------------------------
 * INIT
 -------------------------------------------*/
